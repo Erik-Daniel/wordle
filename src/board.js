d1a70ad6e65fd0995ca7522959cfdaf9b1c5568e
Gavle-hub <erikdaniel650@gmail.com>
2022-03-20 23:56:20 -0700

    added color to boards based on status

--- src/board.js
+++ src/board.js
@@ -8,12 +8,12 @@ import "./boardStyle.css"
 export default function Board() {
     const [getLetters, setLetters] = useState([])
     const [getCurrentLetters, setCurrentLetters] = useState([]);
+    const [getClassNames, setClassNames] = useState([]);
     const [getWord, setWord] = useState("");
     const [getStatus, setStatus] = useState("");
 
     useEffect(() => {
         setWord(word);
-        
     }, []);
 
     function handleKeyPress() {
@@ -53,17 +53,36 @@ export default function Board() {
                 else {
                     let givenWords = getCurrentLetters;
                     let oneWord = "";
+                    let array = [...getClassNames]
                     for(let i = 0; i < givenWords.length; i++){
-                        oneWord = oneWord + givenWords[i];
-                        console.log(getLetters[i])
+                        
+                        if(givenWords[i] === getWord.charAt(i)){
+                            array.push("correct");
+                            
+                        }
+                        else {
+                            let isThereLetter = false;
+                            for(let z = 0; z < givenWords.length; z++){
+                                if(givenWords[i] === getWord.charAt[z]){
+                                    array.push("correct-wrong-p");
+                                    isThereLetter = true;
+                                    break;
+                                }
+                            }
+                            if(!isThereLetter){
+                                array.push("wrong");
+                            }
+                        }
+                        setClassNames(array)
                     }
-                    console.log(getWord)
-                    if(oneWord.toString() === getWord.toString()){
-                        setStatus("You Won!");
-                    }
-                    else {
-                        setStatus("You Lost!");
-                    }
+                    console.log(getClassNames)
+                    // console.log(getWord)
+                    // if(oneWord.toString() === getWord.toString()){
+                    //     setStatus("You Won!");
+                    // }
+                    // else {
+                    //     setStatus("You Lost!");
+                    // }
                     setCurrentLetters([]);
                 }    
             }
@@ -73,42 +92,44 @@ export default function Board() {
   return (
     <div>
         <div className='container'>
-            <div className='boardContainer'>
-                <div className='board'>{getLetters[0]}</div>
-                <div className='board'>{getLetters[1]}</div>
-                <div className='board'>{getLetters[2]}</div>
-                <div className='board'>{getLetters[3]}</div>
-                <div className='board'>{getLetters[4]}</div>
+
+        
+             <div className='boardContainer'>
+                <div className={`board ${getClassNames[0] === undefined ? "" : getClassNames[0]}`}>{getLetters[0]}</div>
+                <div className={`board ${getClassNames[1] === undefined ? "" : getClassNames[1]}`}>{getLetters[1]}</div>
+                <div className={`board ${getClassNames[2] === undefined ? "" : getClassNames[2]}`}>{getLetters[2]}</div>
+                <div className={`board ${getClassNames[3] === undefined ? "" : getClassNames[3]}`}>{getLetters[3]}</div>
+                <div className={`board ${getClassNames[4] === undefined ? "" : getClassNames[4]}`}>{getLetters[4]}</div>
             </div>
             <div className='boardContainer'>
-                <div className='board'>{getLetters[5]}</div>
-                <div className='board'>{getLetters[6]}</div>
-                <div className='board'>{getLetters[7]}</div>
-                <div className='board'>{getLetters[8]}</div>
-                <div className='board'>{getLetters[9]}</div>
+                <div className={`board ${getClassNames[5] === undefined ? "" : getClassNames[5]}`}>{getLetters[5]}</div>
+                <div className={`board ${getClassNames[6] === undefined ? "" : getClassNames[6]}`}>{getLetters[6]}</div>
+                <div className={`board ${getClassNames[7] === undefined ? "" : getClassNames[7]}`}>{getLetters[7]}</div>
+                <div className={`board ${getClassNames[8] === undefined ? "" : getClassNames[8]}`}>{getLetters[8]}</div>
+                <div className={`board ${getClassNames[9] === undefined ? "" : getClassNames[9]}`}>{getLetters[9]}</div>
             </div>
             <div className='boardContainer'>
-                <div className='board'>{getLetters[10]}</div>
-                <div className='board'>{getLetters[11]}</div>
-                <div className='board'>{getLetters[12]}</div>
-                <div className='board'>{getLetters[13]}</div>
-                <div className='board'>{getLetters[14]}</div>
+                <div className={`board ${getClassNames[10] === undefined ? "" : getClassNames[10]}`}>{getLetters[10]}</div>
+                <div className={`board ${getClassNames[11] === undefined ? "" : getClassNames[11]}`}>{getLetters[11]}</div>
+                <div className={`board ${getClassNames[12] === undefined ? "" : getClassNames[12]}`}>{getLetters[12]}</div>
+                <div className={`board ${getClassNames[13] === undefined ? "" : getClassNames[13]}`}>{getLetters[13]}</div>
+                <div className={`board ${getClassNames[14] === undefined ? "" : getClassNames[14]}`}>{getLetters[14]}</div>
             </div>
             <div className='boardContainer'>
-                <div className='board'>{getLetters[15]}</div>
-                <div className='board'>{getLetters[16]}</div>
-                <div className='board'>{getLetters[17]}</div>
-                <div className='board'>{getLetters[18]}</div>
-                <div className='board'>{getLetters[19]}</div>
+                <div className={`board ${getClassNames[15] === undefined ? "" : getClassNames[15]}`}>{getLetters[15]}</div>
+                <div className={`board ${getClassNames[16] === undefined ? "" : getClassNames[16]}`}>{getLetters[16]}</div>
+                <div className={`board ${getClassNames[17] === undefined ? "" : getClassNames[17]}`}>{getLetters[17]}</div>
+                <div className={`board ${getClassNames[18] === undefined ? "" : getClassNames[18]}`}>{getLetters[18]}</div>
+                <div className={`board ${getClassNames[19] === undefined ? "" : getClassNames[19]}`}>{getLetters[19]}</div>
             </div>
             <div className='boardContainer'>
-                <div className='board'>{getLetters[20]}</div>
-                <div className='board'>{getLetters[21]}</div>
-                <div className='board'>{getLetters[22]}</div>
-                <div className='board'>{getLetters[23]}</div>
-                <div className='board'>{getLetters[24]}</div>
+                <div className={`board ${getClassNames[20] === undefined ? "" : getClassNames[20]}`}>{getLetters[20]}</div>
+                <div className={`board ${getClassNames[21] === undefined ? "" : getClassNames[21]}`}>{getLetters[21]}</div>
+                <div className={`board ${getClassNames[22] === undefined ? "" : getClassNames[22]}`}>{getLetters[22]}</div>
+                <div className={`board ${getClassNames[23] === undefined ? "" : getClassNames[23]}`}>{getLetters[23]}</div>
+                <div className={`board ${getClassNames[24] === undefined ? "" : getClassNames[24]}`}>{getLetters[24]}</div>
             </div>
-        </div>
+        </div> 
         <h1>{getWord}</h1>
         <h1>{getStatus}</h1>
     </div>
